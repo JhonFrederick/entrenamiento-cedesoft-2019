9543f4b22b040514d27ec6c8a1cbd51e041c4e72
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-08-16 14:05:01 -0500

    cambios a reservas y usuarios

--- reserva_espacios/apps/usuarios/urls.py
+++ reserva_espacios/apps/usuarios/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('inicio/', inicio, name='inicio'),
     path('registro/', registro, name='registro'),
     path('registro/estudiante', registro_estudiante, name='registro_estudiante'),
-    path('consulta', consulta_usuario, name='consulta_u'),
+    #path('consulta', consulta_usuario, name='consulta_u'),
     path('login/', login_view, name = 'login'),
     path('logout/', logout_view, name = 'logout'),
 ]

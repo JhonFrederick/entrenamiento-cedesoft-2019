032afb5560847dd667621d7902c6e07af19fcae8
JuanDavid69 <juan.gomez.orozco@correounivalle.edu.co>
2019-08-16 10:50:27 -0500

    imagen espacio

--- reserva_espacios/reserva_espacios/urls.py
+++ reserva_espacios/reserva_espacios/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),  #<-- Solo usar para pruebas iniciales
     path('usuarios/', include('apps.usuarios.urls', namespace='usuarios')),
     path('espacios/', include('apps.espacios.urls', namespace='espacios'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

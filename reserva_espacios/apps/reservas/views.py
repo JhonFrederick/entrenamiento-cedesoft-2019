26ada35b7fc2db06f83c46e5b6829a0b838b2cf4
Luis Restrepo <luis.david.restrepo@correounivalle.edu.co>
2019-08-30 15:18:01 -0500

    pulling branch

--- reserva_espacios/apps/reservas/views.py
+++ reserva_espacios/apps/reservas/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioRegistroReserva
 from django.contrib import messages
+from .models import Reserva
 
 
 # Create your views here.
 def solicitar_view(request):
     if request.method == 'POST':
-        form = FormularioRegistroReserva(request.POST)
+        form = FormularioRegistroReserva(request.POST, initial={'estado': Reserva.EN_ESPERA})
         if form.is_valid():
             form.save()
             messages.success(request, "Se creo la reserva exitosamente")
